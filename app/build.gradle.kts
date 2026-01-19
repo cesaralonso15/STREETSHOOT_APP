ac0d5bc054e4469cb27716acc129cff623ed927b
ElPetolo <hectorcuellarmario@gmail.com>
2026-01-19 10:27:30 +0100

    STREET-XX: layouts inicio sesión y registro

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,8 @@ android {
 }
 
 dependencies {
-
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
